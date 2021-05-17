ffdbe73dffa46e5e0cfe1d7bd14a838356132ec8
CXwudi <cx2010017@gmail.com>
2021-05-17 19:50:44 -0400

    🎊 init

---
+++ vocadb-apiclient-fixer/src/main/kotlin/mikufan/cx/vocadbapiclientfixer/VocaDbApiClientFixerApplication.kt
@@ -0,0 +1,11 @@
+package mikufan.cx.vocadbapiclientfixer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VocaDbApiClientFixerApplication
+
+fun main(args: Array<String>) {
+	runApplication<VocaDbApiClientFixerApplication>(*args)
+}
---
+++ vocadb-apiclient-fixer/src/test/kotlin/mikufan/cx/vocadbapiclientfixer/VocaDbApiClientFixerApplicationTests.kt
@@ -0,0 +1,13 @@
+package mikufan.cx.vocadbapiclientfixer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VocaDbApiClientFixerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
