712fb642b600e25c9000f72a8653359da26afc4d
CXwudi <cx2010017@gmail.com>
2022-08-02 15:55:14 -0400

    fix cleaning

--- vocadb-apiclient-fixer/src/main/kotlin/mikufan/cx/vocadbapiclientfixer/component/OutputDirectoryCleaner.kt
+++ vocadb-apiclient-fixer/src/main/kotlin/mikufan/cx/vocadbapiclientfixer/component/OutputDirectoryCleaner.kt
@@ -4,8 +4,8 @@ import mikufan.cx.inlinelogging.KInlineLogging
 import mikufan.cx.vocadbapiclientfixer.config.IOConfig
 import org.springframework.stereotype.Component
 import java.nio.file.Files
-import kotlin.io.path.Path
-import kotlin.io.path.deleteIfExists
+import java.nio.file.Path
+import kotlin.io.path.*
 
 /**
  * @date 2022-08-02
@@ -20,11 +20,25 @@ class OutputDirectoryCleaner(
   fun clean() {
     Files.walk(outputDirectory)
       .parallel()
-      .filter { it != Path(".git") }
+      .filter { it.contains(Path(".git")) && it != outputDirectory }
       .forEach {
-        it.deleteIfExists()
+        deleteDirRecursively(it)
       }
     log.info { "Cleaned up existing files in $outputDirectory except .git" }
+  }
+
+  fun deleteDirRecursively(path: Path) {
+    if (path.isDirectory()) {
+      val contents = path.listDirectoryEntries()
+      if (contents.isNotEmpty()) {
+        for (f in contents) {
+          if (!f.isSymbolicLink()) {
+            deleteDirRecursively(f)
+          }
+        }
+      }
+    }
+    path.deleteIfExists()
   }
 }
 
